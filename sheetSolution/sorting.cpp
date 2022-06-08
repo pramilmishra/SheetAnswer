60e96414cf477c516196049b2f96167a6a3c8b8e
Pramil Mishra <86345034+pramilmishra@users.noreply.github.com>
2022-06-08 13:57:14 +0530

    Add files via upload

---
+++ sheetSolution/sorting.cpp
@@ -0,0 +1,51 @@
+// Given an array of n elements, where each element is at most k away from its target position, you need to sort the array optimally.
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to return the sorted array.
+    vector <int> nearlySorted(int arr[], int num, int K){
+        // Your code here
+        vector<int>ans;
+        for(int i=0; i<num; i++){
+            ans.push_back(arr[i]);
+        }
+        sort(ans.begin(), ans.end());
+        
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+	int T;
+	cin>> T;
+	
+	while (T--)
+	{
+	    int num, K;
+	    cin>>num>>K;
+	    
+	    int arr[num];
+	    for(int i = 0; i<num; ++i){
+	        cin>>arr[i];
+	    }
+	    Solution ob;
+	    vector <int> res = ob.nearlySorted(arr, num, K);
+	    for (int i = 0; i < res.size (); i++)
+	        cout << res[i] << " ";
+	        
+	    cout<<endl;
+	}
+	
+	return 0;
+}
+  // } Driver Code Ends
---
+++ sheetSolution/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+// Given an array of n elements, where each element is at most k away from its target position, you need to sort the array optimally.
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
